13c2fb9dc85bf95d0cb74accdd1eb5d3da38c26c
annabelfast <165572390+annabelfast@users.noreply.github.com>
2025-04-07 14:53:35 +0000

    bite

--- maina.cpp
+++ maina.cpp
@@ -5,16 +5,16 @@
 
 
 int main(){
-  DataBank db("STATION/stations.csv","donnees.csv");
+  Databank db("STATION/stations.csv","donnees.csv");
   RadarImage radar("radar/2024-11-20.tif");
   Date date(2024,11,20);
 
   for (auto it = db.begin(); it != db.end(); ++it){
     Station station = *it;
     float rain_station = db.getRainfall(Station,date);
-    float rain_radar = radar.getRainfallAtCoordinates(station.getLatitude(),station.getLongitude());
+    float rain_radar = radar.getRainfallAtCoordinates(station.getLAT(),station.getLON());
 
-   std::cout << "Station: " << station.getName() << ", Rainfall (Station): " << rain_station << " mm, Rainfall (Radar): " << rain_radar << " mm" << std::endl;
+   std::cout << "Station: " << station.getNOM_USUEL() << ", Rainfall (Station): " << rain_station << " mm, Rainfall (Radar): " << rain_radar << " mm" << std::endl;
   }
 
   return 0;
