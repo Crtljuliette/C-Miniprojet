cf719f9ef54a567a0536421584511741899fd74b
annabelfast <165572390+annabelfast@users.noreply.github.com>
2025-04-07 14:41:47 +0000

    bite

--- testjourdonné/maintestjour.cpp
+++ testjourdonné/maintestjour.cpp
@@ -3,20 +3,20 @@
 #include "Ressources-20250407/RadarImage.h"
 #include "DATABANK/Databank.h"
 
-
+/*
 int main(){
-  DataBank db("STATION/stations.csv","donnees.csv");
+  Databank db("STATION/stations.csv","donnees.csv");
   RadarImage radar("radar/2024-11-20.tif");
   Date date(2024,11,20);
 
-  for (auto it = db.begin(); it != db.end(); ++it){
+  for (auto it = db.begin(); it != db.end(); ++it)
     Station station = *it;
-    float rain_station = db.getRainfall(Station,date);
+    float rain_station = db.getRainfall(station,date);
     float rain_radar = radar.getRainfallAtCoordinates(station.getLatitude(),station.getLongitude());
 
    std::cout << "Station: " << station.getName() << ", Rainfall (Station): " << rain_station << " mm, Rainfall (Radar): " << rain_radar << " mm" << std::endl;
   }
 
-  return 0;
-
+  return 0;  
+ 
 }
